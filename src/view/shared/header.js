d7cf90947fdd0555444d229985d6bf489077acb2
Taras Danyliuk <taras.danylyuk@coaxsoft.com>
2019-09-23 12:19:19 +0300

    move images to another location

--- src/view/shared/header.js
+++ src/view/shared/header.js
@@ -3,7 +3,7 @@ import React from "react";
 const Header = () => {
   return (
     <div className="header">
-      <img className="header--logo" src="assets/images/React.js_logo.png" />
+      <img className="header--logo" src="/images/React.js_logo.png" />
 
       <p className="header--title">COAX Software</p>
     </div>
