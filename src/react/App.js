67351c74de7882695490928f9ccfc060fad8f975
Taras Danyliuk <taras.danyluyk@coaxsoft.com>
2020-04-30 21:36:33 +0300

    remove suspense

--- src/react/App.js
+++ src/react/App.js
@@ -1,39 +1,36 @@
-import React, { Suspense } from "react";
+import React from "react";
 import { Switch } from "react-router-dom";
 
 import GeneralLayout from "./shared/layouts/GeneralLayout";
 
 import routes from "./routes";
-import Loading from "./shared/Loading";
 
 
 function App() {
   return (
-    <Suspense fallback={<Loading/>}>
-      <Switch>
-        {
-          routes.map(route => {
-            return (
-              <route.route
-                key={route.path}
-                path={route.path}
-                exact={route.exact}
-                roles={route.roles}
-                component={props => {
-                  const LayoutComponent = route.layout ? route.layout : GeneralLayout;
+    <Switch>
+      {
+        routes.map(route => {
+          return (
+            <route.route
+              key={route.path}
+              path={route.path}
+              exact={route.exact}
+              roles={route.roles}
+              component={props => {
+                const LayoutComponent = route.layout ? route.layout : GeneralLayout;
 
-                  return (
-                    <LayoutComponent>
-                      <route.component {...props}/>
-                    </LayoutComponent>
-                  )
-                }}
-              />
-            )
-          })
-        }
-      </Switch>
-    </Suspense>
+                return (
+                  <LayoutComponent>
+                    <route.component {...props}/>
+                  </LayoutComponent>
+                )
+              }}
+            />
+          )
+        })
+      }
+    </Switch>
   );
 }
 
